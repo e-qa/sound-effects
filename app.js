28b16e87c33681a11798c25bd23347c0b400319f
Ali <eliqarayev835@gmail.com>
2024-08-14 23:14:01 +0400

    Add audio  effects

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const stopRecord = document.getElementById("stop-record");
 const playback = document.getElementById("playback");
 const download = document.getElementById("download");
 const container = document.getElementById("container");
+const recording = document.getElementById("recording");
+const reverb = document.getElementById("reverb");
+const delay = document.getElementById("delay");
+const chorus = document.getElementById("chorus");
+const distortion = document.getElementById("distortion");
 const userMedia = new Tone.UserMedia();
 
+let audioSrc;
 let recorder;
+let player;
+let currentEffect = null;
 
 record.addEventListener("click", async () => {
   await userMedia.open();
@@ -15,6 +23,7 @@ record.addEventListener("click", async () => {
 });
 
 function startRecording() {
+  recording.textContent = "Recording";
   recorder.start();
 }
 
@@ -24,11 +33,58 @@ stopRecord.addEventListener("click", async () => {
     const url = URL.createObjectURL(recording);
     createAudioEL(url);
   }
+  recording.textContent = "";
 });
 
 function createAudioEL(src) {
   const audio = document.createElement("audio");
   audio.src = src;
   audio.controls = true;
-  container.append(audio);
+  audioSrc = src;
+  player = new Tone.Player(audioSrc).toDestination();
 }
+
+function applyEffect(effect) {
+  if (currentEffect) {
+    player.disconnect(currentEffect);
+  }
+  player.connect(effect);
+  currentEffect = effect;
+  player.start();
+}
+
+reverb.addEventListener("click", async () => {
+  const reverb = new Tone.JCReverb({
+    roomSize: 0.8,
+    wet: 0.5,
+  }).toDestination();
+  applyEffect(reverb);
+});
+
+chorus.addEventListener("click", async () => {
+  const chorusEffect = new Tone.Chorus({
+    frequency: 10,
+    delayTime: 0.1,
+    depth: 0.9,
+    spread: 180,
+  }).toDestination();
+  applyEffect(chorusEffect);
+});
+
+distortion.addEventListener("click", async () => {
+  const distort = new Tone.Distortion({
+    distortion: 0.8,
+    saturate: 0.5,
+  }).toDestination();
+  applyEffect(distort);
+});
+
+delay.addEventListener("click", async () => {
+  const delayEffect = new Tone.FeedbackDelay({
+    delayTime: "8n",
+    feedback: 0.6,
+    wet: 0.5,
+  }).toDestination();
+
+  applyEffect(delayEffect);
+});
